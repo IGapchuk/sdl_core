206361371c578a48440c7a51634ace0f00f94414
LitvinenkoIra <ilytvynenko@luxoft.com>
2018-11-01 16:44:57 +0200

    Add LastStateWrapper interface and LastStateWrapperPtr typedef

--- src/components/transport_manager/include/transport_manager/transport_manager_default.h
+++ src/components/transport_manager/include/transport_manager/transport_manager_default.h
@@ -38,10 +38,6 @@
 
 #include "transport_manager/transport_manager_impl.h"
 
-namespace resumption {
-class LastStateWrapper;
-}
-
 namespace transport_manager {
 
 struct TransportAdapterFactory {
@@ -80,8 +76,7 @@ class TransportManagerDefault : public TransportManagerImpl {
    *
    * @return Code error.
    */
-  int Init(std::shared_ptr<resumption::LastStateWrapper> last_state_wrapper)
-      OVERRIDE;
+  int Init(resumption::LastStateWrapperPtr last_state_wrapper) OVERRIDE;
 
   /**
    * @brief Destructor.
